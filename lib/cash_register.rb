bdb303458a6b0512a6e61f6c828a13f7b9eb9be9
Di Zheng <dizhengcindy@gmail.com>
2020-03-11 17:33:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+require 'pry'
+class CashRegister
+  attr_accessor:discount,:total
+  def initialize(d)
+  @total = 0 
+  @discount = d 
+  end
 
+  def total=(total)
+    self.total = total
+  end
+
+  def add_item(title,price, q=1)
+    self.total += price*q
+  end
+  def apply_discount
+    if @discount>0
+       @total = @total*(1-@discount.to_f/100)
+     "After the discount, the total comes to $#{total.to_i}."
+     else
+     "There is no discount to apply."
+    end
+  end
+
+
+
+end
